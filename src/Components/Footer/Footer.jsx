ee0d60d74df4bcb38add1f1977b1e615e279a2a5
yehuhi <email@example.com>
2024-03-17 18:06:37 +0200

    new 3.0

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
+import logo from '../Assets/logo2.png';
+import { Link } from 'react-router-dom';
+import { BsInstagram } from 'react-icons/bs';
+import { FaTiktok } from 'react-icons/fa6';
+import { FaFacebookSquare } from 'react-icons/fa';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div style={{width:"100%", height:"250px",display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", color:"white", backgroundColor:"grey"}}>
-      FOOTER
-    </div>
-  )
-}
+    <div className="footer-container">
+      <img className="footer-img" src={logo} alt="logo" />
+      <div className="footer-txt">
+        <div className="texts">
+          <span className="txt-span">
+            Somos una tienda de ropa importada con precios competitivos de las
+            mejores marcas internacionales para todas las edades.
+          </span>
+          <span className="txt-span">
+            Tambien recibimos pedidos de productos que no encuentren en nuestar
+            pagina web.
+          </span>
+        </div>
 
-export default Footer
+        <div className="footer-links">
+          <div className="link-foo">
+            <Link to={'/womens'}>Mujeres</Link>
+          </div>
+          <div className="link-foo">
+            <Link to={'/mens'}>Hombres</Link>
+          </div>
+          <div>
+            <Link to={'/kids'}>Ninos</Link>
+          </div>
+        </div>
+      </div>
+      <div className="icons-footer">
+        <div className="icons-foo">
+          <BsInstagram />
+        </div>
+        <div className="icons-foo">
+          <FaTiktok />
+        </div>
+        <div className="icons-foo">
+          <FaFacebookSquare />
+          <div />
+        </div>
+      </div>
+      <div />
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Hero2/Hero2.jsx
+++ src/Components/Hero2/Hero2.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './Hero2.css';
 
 const Hero2 = () => {
   return (
-    <div style={{width:"100%", height:"500px", backgroundColor:"green"}}>
-      
-    </div>
-  )
-}
+    <div className="hero-cont2">
+      <div className="div_p">
+        <p className="p1">LA GUIA DE REGALOS PARA EL DIA DE SAN VALENTIN</p>
+        <p className="p2">
+          Celebra el Día de San Valentín con colores vivos, looks apasionantes
+          para una cita nocturna y regalos a juego, como nuestro nuevo bolso
+          Tribeca.
+        </p>
+      </div>
 
-export default Hero2
+      <div className="images-hero">
+        <div className="image-hero1">
+          <img
+            className="img-hero"
+            src="https://www.beyoung.in/blog/wp-content/uploads/2023/01/The-Shoulder-Bag-853x1024.jpg"
+            alt=""
+          />
+        </div>
+        <div className="image-hero1">
+          <img
+            className="img-hero"
+            src="https://5.imimg.com/data5/SELLER/Default/2023/6/317537012/JU/CL/VG/185931781/zjmc10-12--500x500.jpg"
+            alt=""
+          />
+        </div>
+        <div className="image-hero1">
+          <img
+            className="img-hero"
+            src="https://www.guess.ae/dw/image/v2/BDDB_PRD/on/demandware.static/-/Sites-guess-master-catalog/default/dw04e7db67/images/BG787924_CLO_05.jpg?sw=800&sh=1040"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero2;
